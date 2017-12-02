941e62694ffd07271ba0c8dd4537e1feb5fda07a
Stefano Coretta <stefanoc.social@me.com>
2017-12-02 18:44:08 +0000

    keep tex

--- R/functions.R
+++ R/functions.R
@@ -6,6 +6,7 @@
 mfil_abstract <- function(...) {
     rmarkdown::pdf_document(
         ...,
+        keep_tex = TRUE,
         template = system.file("rmarkdown/templates/mfil_abstract/resources", "template.tex", package = "mfiltemplate"),
         latex_engine = "xelatex"
     )
