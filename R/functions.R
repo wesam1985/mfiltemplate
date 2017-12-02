4d135f2f5a6157aa4214bd45c673c9b8586b1d30
Stefano Coretta <stefanoc.social@me.com>
2017-12-02 17:47:57 +0000

    prepare template
    Not functioning yet :exclamation:

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' mFiL abstract format.
+#'
+#' Format for creating an abstract for submission to mFiL.
+#'
+#' @export
+mfil_abstract <- function() {
+    rmarkdown::pdf_document(
+        template = system.file("rmarkdown/templates/mfil_abstract/resources", "template.tex", package = "mfiltemplate"),
+        latex_engine = "xelatex"
+    )
+}
