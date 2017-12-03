2f2e129c24f18d8e6e2589ce9b30b8515d555fc6
Stefano Coretta <stefanoc.social@me.com>
2017-12-03 13:36:48 +0000

    move template find to other line

--- R/functions.R
+++ R/functions.R
@@ -4,10 +4,13 @@
 #'
 #' @export
 mfil_abstract <- function(...) {
+
+mfil_template <- system.file("rmarkdown/templates/mfil_abstract/resources", "template.tex", package = "mfiltemplate")
+
     rmarkdown::pdf_document(
         ...,
         keep_tex = TRUE,
-        template = system.file("rmarkdown/templates/mfil_abstract/resources", "template.tex", package = "mfiltemplate"),
+        template = mfil_template,
         latex_engine = "xelatex"
     )
 }
